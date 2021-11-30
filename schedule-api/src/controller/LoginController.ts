1203e6808feb3b2e8bc490b5f97e65e003d8f9d5
sivsankar <sivasankar.devireddy@xgeniesoft.com>
2021-11-30 08:23:38 +0530

    login api changes

--- schedule-api/src/controller/LoginController.ts
+++ schedule-api/src/controller/LoginController.ts
@@ -7,6 +7,7 @@ import { TeacherAvailability } from "../entity/TeacherAvailability";
 import { getManager } from "typeorm";
 import { USERS } from "../data/users";
 import { JWSTokenHandler } from "../helpers/JWSTokenHandler";
+import { Admin } from "../entity/Admin";
 
 export class LoginController {
 
@@ -14,65 +15,18 @@ export class LoginController {
     private leadAvailabilityRepository = getRepository(TeacherAvailability);
     private leadRepository = getRepository(Teacher);
     private userRepository = getRepository(User);
+    private adminRepository = getRepository(Admin);
 
     async login(request: Request, response: Response, next: NextFunction) {
         const req = request.body;
-
-        const cookies = request.signedCookies;
-        const token = cookies['qe-admin-token'];
-        if (token) {
-            console.log('token', token);
-            const decodedToken = new JWSTokenHandler().decode(token);
-            console.log('decodedToken', decodedToken);
-            const tokenPayload = JSON.parse(decodedToken.payload || '{}'); 
-            const foundUser = USERS.find((_u) => _u.phone === tokenPayload.phone && _u.code === tokenPayload.code);
-            if (foundUser) {
-                return response.status(200).send({
-                    status: "ok",
-                    type: "mobile",
-                    currentAuthority: foundUser.currentAuthority
-                }).end();
-            }
-        }
-
-        if (req.type !== 'mobile') {
-            //return 400
-            return response.status(400).end()
-        }
-        if (!req.mobile || !req.captcha) {
-            // return 400
-            return response.status(400).end()
-        }
-
-        const foundUser = USERS.find((_u) => _u.phone === req.mobile && _u.code === req.captcha);
-        if (!foundUser) {
-            // return 401
-            return response.status(401).send({
-                message: 'Invalid phone/code provided'
-            }).end()
-        }
-
-        const tokenPayload = {
-            phone: foundUser.phone,
-            code: foundUser.code,
-            expiry: (new Date().getTime() + (24 * 60 * 60))
-        };
-        const sessionToken = new JWSTokenHandler().signToken(JSON.stringify(tokenPayload));
-
-        let options = {
-            maxAge: 1000 * 60 * 60 * 24, // would expire after 1 day
-            httpOnly: true,
-            signed: true
-        }
-    
-        response.cookie('qe-admin-token', sessionToken, options)
-
-        return response.status(200).send({
-            status: "ok",
-            type: "mobile",
-            currentAuthority: foundUser.currentAuthority,
-            token: sessionToken
-        }).end();
+        let userToRemove = await this.adminRepository.find({select:["firstname","lastname","email","phone"] ,where: { email: req.email, password: req.password} }); 
+        if (userToRemove.length>0) {
+   
+           return {"success":true,"data": userToRemove, "total":1, "current":1, pageSize:1};
+        }   else {
+           return {"success":false,"data": userToRemove, "total":1, "current":1, pageSize:1};
+        }   
+  
     }
 
     async currentUser(request: Request, response: Response, next: NextFunction) {
