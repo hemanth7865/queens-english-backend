23c44c666185d8ef5ba14ca6d5057e7f4dcb0ecd
ravi <ravi@queensenglish.co>
2021-12-17 23:22:33 +0530

    Attendance API

---
+++ schedule-api/src/controller/SessionController.ts
@@ -0,0 +1,97 @@
+import { NextFunction, Request, Response } from "express";
+import { QueryFailedError } from "typeorm";
+import { Session } from "../entity/Session";
+import { SessionService } from "../services/SessionService";
+const cron = require("node-cron");
+
+export class SessionController {
+  private SessionService = new SessionService();
+
+  async createSession(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    var session: Session;
+    console.log("Saving Session");
+    try {
+      session = await this.SessionService.createSession(request.body);
+    } catch (error) {
+      return { success: false, error: error.toString() };
+    }
+    return { success: true, sessionId: session };
+  }
+
+  async updateSession(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    var session: Session;
+    console.log("Updating Session");
+    try {
+      session = await this.SessionService.updateSession(request.params.id, request.body);
+    } catch (error) {
+      return { success: false, error: error.toString() };
+    }
+    return { success: true, session: session };
+  }
+
+  async getBatchSessions(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    var session: Session[] = null;
+    try {
+      session = await this.SessionService.getBatchSessions(request.params.id);
+      if (session == null || session.length == 0) {
+        return { success: false, message: "Session doesn't exist" };
+      }
+    } catch (error) {
+      console.log(error);
+      return { success: false, message: "Unknown Error fetching session details" };
+    }
+    return { success: true, session: session };
+  }
+
+  async getBatchLessonSession(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    var session: Session = null;
+    try {
+      var {id, lessonId} = request.params;
+      session = await this.SessionService.getBatchLessonSession(id, lessonId);
+      if (session == null) {
+        return { success: false, message: "Session doesn't exist" };
+      }
+    } catch (error) {
+      console.log(error);
+      return { success: false, message: "Unknown Error fetching session details" };
+    }
+    return { success: true, session: session };
+  }
+
+
+  async getSessions(request: Request, response: Response, next: NextFunction) {
+    var res: Session[];
+    try {
+      res = await this.SessionService.getSessions();
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getSessionDetail(request: Request, response: Response, next: NextFunction) {
+    var res: Session;
+    try {
+      res = await this.SessionService.getSessionDetail(request.params.id);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
--- schedule-api/src/routes.ts
+++ schedule-api/src/routes.ts
@@ -1,100 +1,158 @@
 import { LoginController } from "./controller/LoginController";
-import {UserController} from "./controller/UserController";
-import { BatchController} from "./controller/BatchController";
+import { UserController } from "./controller/UserController";
+import { BatchController } from "./controller/BatchController";
+import { SessionController } from "./controller/SessionController";
 
 export const Routes = [
-    {
-        method: "post",
-        route: "/login",
-        controller: LoginController,
-        action: "login"
-    },
-    {
-        method: "get",
-        route: "/logout",
-        controller: LoginController,
-        action: "logout"
-    },
-    {
-        method: "get",
-        route: "/lessons",
-        controller: UserController,
-        action: "lessons"
-    },
-    {
-        method: "get",
-        route: "/currentUser",
-        controller: LoginController,
-        action: "currentUser"
-    },
-    {
+  {
+    method: "post",
+    route: "/login",
+    controller: LoginController,
+    action: "login",
+  },
+  {
+    method: "get",
+    route: "/logout",
+    controller: LoginController,
+    action: "logout",
+  },
+  {
+    method: "get",
+    route: "/lessons",
+    controller: UserController,
+    action: "lessons",
+  },
+  {
+    method: "get",
+    route: "/currentUser",
+    controller: LoginController,
+    action: "currentUser",
+  },
+  {
     method: "get",
     route: "/leads",
     controller: UserController,
-    action: "allLeads"
-},
+    action: "allLeads",
+  },
 
-{
+  {
     method: "get",
     route: "/leadsView",
     controller: UserController,
-    action: "listLeadDetails"
-}, {
+    action: "listLeadDetails",
+  },
+  {
     method: "get",
     route: "/leadsFullView/:id",
     controller: UserController,
-    action: "leadFullDetails"
-},
-{
+    action: "leadFullDetails",
+  },
+  {
     method: "post",
     route: "/leads",
     controller: UserController,
-    action: "saveLeads"
-}, {
+    action: "saveLeads",
+  },
+  {
     method: "get",
     route: "/filterLead",
     controller: UserController,
-    action: "filterLeadDetails"
-}, {
+    action: "filterLeadDetails",
+  },
+  {
     method: "delete",
     route: "/users/:id",
     controller: UserController,
-    action: "remove"
-},
-{
+    action: "remove",
+  },
+  {
     method: "delete",
     route: "/batch/:id",
     controller: BatchController,
-    action: "remove"
-},
-{
+    action: "remove",
+  },
+  {
     method: "post",
     route: "/createBatch",
     controller: BatchController,
-    action: "createBatch"
-},
-{
+    action: "createBatch",
+  },
+  {
     method: "get",
     route: "/listBatch",
     controller: BatchController,
-    action: "listBatch"
-},
-{
+    action: "listBatch",
+  },
+  {
     method: "get",
     route: "/listclass",
     controller: BatchController,
-    action: "getClasses"
-},
-{
+    action: "getClasses",
+  },
+  {
     method: "get",
     route: "/runBatchJob",
     controller: BatchController,
-    action: "runBatchJob"
-},
-{
+    action: "runBatchJob",
+  },
+  {
     method: "get",
     route: "/listBatch/:id",
     controller: BatchController,
-    action: "getBatchDetails"
-}
+    action: "getBatchDetails",
+  },
+  {
+    method: "delete",
+    route: "/batch/:id",
+    controller: BatchController,
+    action: "remove",
+  },
+  {
+    method: "post",
+    route: "/createBatch",
+    controller: BatchController,
+    action: "createBatch",
+  },
+  {
+    method: "get",
+    route: "/listBatch",
+    controller: BatchController,
+    action: "listBatch",
+  },
+  {
+    method: "get",
+    route: "/session/:id",
+    controller: SessionController,
+    action: "getSessionDetail",
+  },
+  {
+    method: "get",
+    route: "/session/batch/:id",
+    controller: SessionController,
+    action: "getBatchSessions",
+  },
+  {
+    method: "get",
+    route: "/session/batch/:id/:lessonId",
+    controller: SessionController,
+    action: "getBatchLessonSession",
+  },
+  {
+    method: "get",
+    route: "/session",
+    controller: SessionController,
+    action: "getSessions",
+  },
+  {
+    method: "post",
+    route: "/session",
+    controller: SessionController,
+    action: "createSession",
+  },
+  {
+    method: "put",
+    route: "/session/:id",
+    controller: SessionController,
+    action: "updateSession",
+  },
 ];
