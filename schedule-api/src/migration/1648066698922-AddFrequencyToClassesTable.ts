990c21994e4991fd69571d1085db0f5c6ee43524
Bishoy Romany <45445067+bishoyromany@users.noreply.github.com>
2022-03-29 10:32:42 +0200

    Added migrations to work fine for MySQL

--- schedule-api/src/migration/1648066698922-AddFrequencyToClassesTable.ts
+++ schedule-api/src/migration/1648066698922-AddFrequencyToClassesTable.ts
@@ -3,11 +3,11 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class AddFrequencyToClassesTable1648066698922 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE classes ADD COLUMN IF NOT EXISTS frequency VARCHAR(45) NULL`);
+        await queryRunner.query(`ALTER TABLE classes ADD frequency VARCHAR(45) NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE classes DROP COLUMN IF EXISTS frequency`);
+        await queryRunner.query(`ALTER TABLE classes DROP COLUMN frequency`);
     }
 
 }
