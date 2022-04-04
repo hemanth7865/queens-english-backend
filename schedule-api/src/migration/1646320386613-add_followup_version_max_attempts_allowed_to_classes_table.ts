24cb5ea95c6dc31730449259d592da4e618eb5ca
Bishoy Romany <45445067+bishoyromany@users.noreply.github.com>
2022-04-04 11:04:32 +0200

    Added columns exit check for migrations

--- schedule-api/src/migration/1646320386613-add_followup_version_max_attempts_allowed_to_classes_table.ts
+++ schedule-api/src/migration/1646320386613-add_followup_version_max_attempts_allowed_to_classes_table.ts
@@ -1,11 +1,17 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class addFollowupVersionMaxAttemptsAllowedToClassesTable1646320386613 implements MigrationInterface {
     name = 'addFollowupVersionMaxAttemptsAllowedToClassesTable1646320386613'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE classes ADD followupVersion VARCHAR(45) NULL`);
-        await queryRunner.query(`ALTER TABLE classes ADD maxAttemptsAllowed INT(11) NULL`);
+        if (!await queryRunner.hasColumn('classes', 'followupVersion')) { 
+            await queryRunner.query(
+            `ALTER TABLE classes ADD followupVersion VARCHAR(45) NULL`
+            );
+        }
+        if (!await queryRunner.hasColumn('classes', 'maxAttemptsAllowed')) { 
+            await queryRunner.query(`ALTER TABLE classes ADD maxAttemptsAllowed INT(11) NULL`);
+        }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
