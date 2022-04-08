888d2aa123cf49a1223d3c338d66664afa8d316f
Bishoy Romany <45445067+bishoyromany@users.noreply.github.com>
2022-04-08 14:02:53 +0200

    update LSQ Body

--- schedule-api/src/services/LQSService.ts
+++ schedule-api/src/services/LQSService.ts
@@ -340,7 +340,12 @@ export class LQSService {
       });
       payment == null ? new Payment() : payment;
 
-      const details = await axios(url)
+      const details = await axios(url, {
+        data: {
+          "Parameter": {"ActivityEvent": 210},
+          "Paging": {"Offset": "0","RowCount": "10"}
+        }
+      })
         .then(async (response) => {
           element.retry = element.retry - 1
           if (response.data) {
