46d1a25b2510fcf7d6e05091727bcef235940a4d
Nagaveni <nagaveni@queensenglish.co>
2022-04-09 14:34:42 +0530

    migration table lsq_users_ID

---
+++ schedule-api/src/migration/1649494845940-AddLsqUserIdTostudentTable.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddLsqUserIdTostudentTable1649494845940 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        if (!await queryRunner.hasColumn('student', 'lsq_users_ID')) { 
+            await queryRunner.query(`ALTER TABLE student ADD lsq_users_ID VARCHAR(200) NULL`);
+        }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        if (!await queryRunner.hasColumn('student', 'lsq_users_ID')) { 
+            await queryRunner.query(`ALTER TABLE student DROP lsq_users_ID`);
+        }
+    }
+    
+
+}
