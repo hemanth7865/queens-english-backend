33599d8e1a2835763529a2ca0aad671e8eccde16
Nagaveni <nagaveni@queensenglish.co>
2022-04-11 18:10:22 +0530

    whatsappLink migration

---
+++ schedule-api/src/migration/1649680669561-addWhatsappLinkToClassesTable.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class addWhatsappLinkToClassesTable1649680669561 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        if (!await queryRunner.hasColumn('classes', 'whatsappLink')) { 
+            await queryRunner.query(`ALTER TABLE classes ADD whatsappLink VARCHAR(200) NULL`);
+        }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE classes DROP COLUMN whatsappLink`);
+    }
+
+}
