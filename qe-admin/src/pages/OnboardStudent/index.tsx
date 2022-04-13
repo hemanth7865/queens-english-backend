def91d5b45002bb19b147047e023bd6e65546da0
Nagaveni <nagaveni@queensenglish.co>
2022-04-13 17:28:51 +0530

    payment value in onboarding tab

--- qe-admin/src/pages/OnboardStudent/index.tsx
+++ qe-admin/src/pages/OnboardStudent/index.tsx
@@ -326,6 +326,7 @@ const StudentOnboard: React.FC = () => {
       timings: value.timings,
       waMessageSent: value.waMessageSent,
       prm_id: String(value.prm).length < 3 && parseInt(value.prm) > 0 ? value.prm : value.prm_id,
+      payments: value.payments
     }
     try {
       const msg = await addTeacherSchedule({
