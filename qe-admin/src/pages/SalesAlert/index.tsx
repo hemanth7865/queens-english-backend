a339a810dbe63b04e6bfcb19925e03ba98d93c25
Nagaveni <nagaveni@queensenglish.co>
2022-05-04 09:08:15 +0530

    removed classType from sales alert view

--- qe-admin/src/pages/SalesAlert/index.tsx
+++ qe-admin/src/pages/SalesAlert/index.tsx
@@ -506,7 +506,7 @@ const StudentOnboard: React.FC = () => {
       var isValidation = false
       for (var key in p) {
           if (p.hasOwnProperty(key)) {
-              if(key == 'lsq_user_name' || key == 'lsq_user_id' ||key == 'prm' ||key == 'prm_id' ||key == 'customerEmail' ||key == 'timings' ||key == 'courseFrequency' ||key == 'firstName' ||key == 'alternativeMobile' ||key == 'course' ||key == 'startLesson' ||key == 'startDate' ||key == 'paymentMode' ||key == 'emiMonths' ||key == 'emi' || key == 'subscription' ||key == 'saleamount' ||key == 'classessold' ||key == 'downpayment' ||key == 'paymentid' ||key == 'classType' ||key == 'address' ||key == 'whatsapp' ||key == 'dob' ||key == 'status' ||key == 'email' ||key == 'phoneNumber'){
+              if(key == 'lsq_user_name' || key == 'lsq_user_id' ||key == 'prm' ||key == 'prm_id' ||key == 'customerEmail' ||key == 'timings' ||key == 'courseFrequency' ||key == 'firstName' ||key == 'alternativeMobile' ||key == 'course' ||key == 'startLesson' ||key == 'startDate' ||key == 'paymentMode' ||key == 'emiMonths' ||key == 'emi' || key == 'subscription' ||key == 'saleamount' ||key == 'classessold' ||key == 'downpayment' ||key == 'paymentid' ||key == 'address' ||key == 'whatsapp' ||key == 'dob' ||key == 'status' ||key == 'email' ||key == 'phoneNumber'){
                   var tempKeyValue = p[key] + ''
                   if(isTempEntryStatus){
                       if(tempKeyValue.length > 0 && tempKeyValue != undefined && tempKeyValue != null){
