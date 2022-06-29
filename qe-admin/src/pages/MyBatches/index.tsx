09898a241cae643d1b8e4d46434c6f9c3c5b3702
Bishoy Romany <bishoy@queensenglish.co>
2022-06-29 19:27:34 +0200

    create basic page that shows batches and links

--- qe-admin/src/pages/MyBatches/index.tsx
+++ qe-admin/src/pages/MyBatches/index.tsx
@@ -1,11 +1,10 @@
-import { Button, Drawer, Spin, Modal } from 'antd';
+import { Spin } from 'antd';
 import React, { useState, useRef } from 'react';
 import { useIntl } from 'umi';
 import { PageContainer } from '@ant-design/pro-layout';
 import type { ActionType } from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
-import { getAllCollectionAgents } from '@/services/ant-design-pro/api';
-import Upload from './Components/Upload';
+import { getAllZoomMeetings } from '@/services/ant-design-pro/api';
 import { columns as columnsMethod } from "./columns";
 
 /**
@@ -15,7 +14,6 @@ import { columns as columnsMethod } from "./columns";
  */
 const TableList: React.FC = () => {
     const actionRef = useRef<ActionType>();
-    const [upload, setUpload] = useState<boolean>(false);
 
     const [isLoading, setIsLoading] = useState(false);
 
@@ -30,37 +28,17 @@ const TableList: React.FC = () => {
                 <ProTable<API.RuleListItem, API.PageParams>
                     headerTitle={intl.formatMessage({
                         id: 'pages.searchTable.titleUser',
-                        defaultMessage: 'Collection Agents',
+                        defaultMessage: 'My Batches',
                     })}
                     actionRef={actionRef}
                     rowKey="key"
                     search={{
                         labelWidth: 120,
                     }}
-                    request={getAllCollectionAgents}
+                    request={getAllZoomMeetings}
                     columns={columns}
-                    toolBarRender={() => [
-                        <Button type="primary" key="primary" onClick={() => setUpload(true)}>
-                            Bulk Assignment
-                        </Button>,
-                    ]
-                    }
                 />
             </Spin>
-
-            <Modal
-                width={700}
-                visible={upload}
-                title={`Upload File`}
-                onCancel={() => {
-                    setUpload(false);
-                }}
-                footer={null}
-                centered
-            >
-                <Upload isLoading={isLoading} setIsLoading={setIsLoading} />
-            </Modal>
-
         </PageContainer>
     );
 };
