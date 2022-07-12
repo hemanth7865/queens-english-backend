c71684e66bae57a471831e6bb34c0cb2badbd3ec
vedantsonkarr <vedant@queensenglish.co>
2022-07-12 11:47:00 +0530

    All typos fixed

--- schedule-api/src/migration/1657523106259-existingStudents.ts
+++ schedule-api/src/migration/1657523106259-existingStudents.ts
@@ -3,14 +3,14 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class existingStudents1657523106259 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        if (!await queryRunner.hasColumn('student', 'prevBathcedStudent')) {
-            await queryRunner.query(`ALTER TABLE student ADD prevBathcedStudent BOOLEAN NOT NULL DEFAULT false`);
+        if (!await queryRunner.hasColumn('student', 'prevBatchedStudent')) {
+            await queryRunner.query(`ALTER TABLE student ADD prevBatchedStudent BOOLEAN NOT NULL DEFAULT false`);
         }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        if (!await queryRunner.hasColumn('student', 'prevBathcedStudent')) {
-            await queryRunner.query(`ALTER TABLE student ADD prevBathcedStudent BOOLEAN NOT NULL DEFAULT false`);
+        if (!await queryRunner.hasColumn('student', 'prevBatchedStudent')) {
+            await queryRunner.query(`ALTER TABLE student ADD prevBatchedStudent BOOLEAN NOT NULL DEFAULT false`);
         }
     }
 
