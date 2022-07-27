b0ba38694177693e7c55bd37956e4e8e5992446d
Nagaveni <nagaveni@queensenglish.co>
2022-07-27 19:50:08 +0530

    Migration for subscription status and cycles

---
+++ schedule-api/src/migration/1658930824711-subscriptionstatusToTransactions.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class subscriptionstatusToTransactions1658930824711 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        if (!(await queryRunner.hasColumn("installments", "subscription_status"))) {
+            await queryRunner.query(
+                `ALTER table installments add subscription_status varchar(100) NOT NULL`
+            );
+        }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        if (await queryRunner.hasColumn("installments", "subscription_status")) {
+            await queryRunner.query(
+                `ALTER table installments add subscription_status varchar(100) NOT NULL`
+            );
+        }
+    }
+
+}
