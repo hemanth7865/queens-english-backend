5bd1d555937992dd292129244369b7d1bbfcfd25
Bishoy Romany <bishoy@queensenglish.co>
2022-09-22 18:05:28 +0200

    Completed generating students join link

--- schedule-api/src/services/UserJoinLinkService.ts
+++ schedule-api/src/services/UserJoinLinkService.ts
@@ -53,48 +53,96 @@ export class UserJoinLinkService {
     try {
       const students = await this.getStudentWithoutCorrectJoinLink();
 
+      logger.info(
+        `UserJoinLinkService::generateStudentsJoinLink start generating links for ${students.length}`
+      );
+
       for (let student of students) {
-        const email = student.user_id + this.emailFormat;
-
-        const registrantUser = {
-          first_name: student.user_firstName,
-          last_name: student.user_lastName,
-          email,
-        };
-
-        const createdRegisterantUser: {
-          id: number;
-          join_url: string;
-          registrant_id: string;
-        } = await zoomClient.addMeetingRegistrant(
-          student.meeting_id,
-          registrantUser
-        );
-
-        if (createdRegisterantUser.registrant_id) {
-          const join_link = {
-            id: student.user_id,
-            join_url: createdRegisterantUser.join_url,
-            registrant_id: createdRegisterantUser.registrant_id,
-            email,
-            meeting_id: student.meeting_id,
-            batch_id: student.batch_id,
-          };
-
-          const createdJoinLink = await this.userJoinLinksRepositroy.save(
-            join_link
+        try {
+          logger.info(
+            `UserJoinLinkService::generateStudentsJoinLink start generating join link for student ${student.user_id}`
           );
 
-          if (createdJoinLink.id) {
-            result.success += 1;
-          }
-        }
+          const email = student.user_id + this.emailFormat;
 
-        break;
+          const registrantUser = {
+            first_name: student.user_firstName,
+            last_name: student.user_lastName,
+            email,
+          };
+
+          const createdRegisterantUser: any =
+            await zoomClient.addMeetingRegistrant(
+              student.meeting_id,
+              registrantUser
+            );
+
+          if (createdRegisterantUser.registrant_id) {
+            const join_link = {
+              id: student.user_id,
+              join_url: createdRegisterantUser.join_url,
+              registrant_id: createdRegisterantUser.registrant_id,
+              email,
+              meeting_id: student.meeting_id,
+              batch_id: student.batch_id,
+            };
+
+            const createdJoinLink = await this.userJoinLinksRepositroy.save(
+              join_link
+            );
+
+            if (createdJoinLink.id) {
+              result.success += 1;
+            }
+
+            logger.info(
+              `UserJoinLinkService::generateStudentsJoinLink completed generating join link for student ${createdJoinLink.id}`
+            );
+
+            continue;
+          }
+
+          throw new Error(
+            `student: ${student.user_id} error: ${createdRegisterantUser?.message}`
+          );
+        } catch (e) {
+          logger.error(e.message);
+          result.errors += 1;
+          await (
+            await this.logger.customZoom(
+              student.user_id,
+              "Failed to generate student join link: " + e.message,
+              "FAILED_TO_GENERATE_STUDENT_JOIN_LINK",
+              { error: e, message: e.message, student },
+              this.request.user || {}
+            )
+          ).save();
+          result.logs.push(e.message);
+        }
       }
     } catch (e) {
+      logger.error(e.message);
+      await (
+        await this.logger.customZoom(
+          "FAILED_TO_GENERATE_STUDENTS_JOIN_LINK_OVERALL",
+          "Failed to generate students join link: " + e.message,
+          "FAILED_TO_GENERATE_STUDENTS_JOIN_LINK_OVERALL",
+          { error: e, message: e.message },
+          this.request.user || {}
+        )
+      ).save();
       result.logs.push(e.message);
     }
+
+    await (
+      await this.logger.customZoom(
+        "GENERATE_STUDENTS_JOIN_LINK_RESULT",
+        "Generate students join link result",
+        "GENERATE_STUDENTS_JOIN_LINK_RESULT",
+        { result },
+        this.request.user || {}
+      )
+    ).save();
 
     return result;
   }
