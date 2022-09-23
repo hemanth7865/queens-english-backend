9f296458885764de70fdf85206fb97c6adc13936
Bishoy Romany <bishoy@queensenglish.co>
2022-09-23 14:16:21 +0200

    store user code in cosmos

--- schedule-api/src/services/UserService.ts
+++ schedule-api/src/services/UserService.ts
@@ -1,6 +1,7 @@
 import { getRepository, Not, IsNull } from "typeorm";
 import { User } from "../entity/User";
 import { generateRandomCode } from "./../utils/batch/getUniqueTeacherCode";
+import axios from "axios";
 const { usersLogger } = require("../Logger.js");
 
 export class UserService {
@@ -69,7 +70,7 @@ export class UserService {
     const result = {
       success: 0,
       error: 0,
-      log: []
+      log: [],
     };
     try {
       const users = await this.usersRepository.find({
@@ -80,12 +81,32 @@ export class UserService {
         try {
           const code = await this.getUniqueCode();
           user.userCode = code;
-          this.usersRepository.update(user.id, user);
-          result.success += 1;
+          const cosmosUpdate = {
+            url: `${process.env.COSMOS_URL}/api/user/?code=${process.env.COSMOS_CODE}`,
+            json: true,
+            body: {
+              userCode: code,
+              id: user.id,
+            },
+          };
+
+          await axios
+            .put(cosmosUpdate.url, cosmosUpdate.body)
+            .then(async (res) => {
+              this.usersRepository.update(user.id, user);
+              result.success += 1;
+              return user;
+            })
+            .catch(async (error) => {
+              throw new Error(
+                "Failed to update user on cosmos " + error.response.data
+              );
+            });
         } catch (e) {
           result.error += 1;
           usersLogger.error(e.message);
           result.log.push(e.mesage);
+          continue;
         }
       }
     } catch (e) {
