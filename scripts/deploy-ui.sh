769b11d06b0ca84e1e6d2b5522ede3375f52da41
Bishoy Romany <bishoy@queensenglish.co>
2022-10-03 11:01:23 +0200

    Temporary fix for nonsense 403 nginx error.

--- scripts/deploy-ui.sh
+++ scripts/deploy-ui.sh
@@ -2,3 +2,4 @@
 
 cd /home/qeuser/qe-backend/qe-admin && npm run build
 rm -rf /var/www/html/qe-admin && cp -r /home/qeuser/qe-backend/qe-admin/dist /var/www/html/qe-admin
+rm -rf /var/www/html/qe-admin-ui && cp -r /var/www/html/qe-admin /var/www/html/qe-admin-ui
