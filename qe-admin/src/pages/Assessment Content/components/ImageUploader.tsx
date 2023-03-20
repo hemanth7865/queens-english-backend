aa3ae8b990d4c0fc2a837c18faf1d52b96f41075
vedantsonkarr <vedant@queensenglish.co>
2023-03-21 02:12:12 +0530

    Removed Console Log

--- qe-admin/src/pages/Assessment Content/components/ImageUploader.tsx
+++ qe-admin/src/pages/Assessment Content/components/ImageUploader.tsx
@@ -69,8 +69,6 @@ const ImageUploader: React.FC<ImageUploaderProps> = (props: any) => {
         setLoading(false);
     };
 
-    console.log("ImageUploader", props.data, props.imageURI)
-
     return (
         <>
             <Spin spinning={loading}>
