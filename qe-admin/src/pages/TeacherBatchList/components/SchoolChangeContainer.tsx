edf6de74f9ac996df12f0591348a3f111f68a512
Manish Prajapati <manish.prajapati@queensenglish.co>
2023-04-03 20:00:07 +0530

    Added school change container for changing schools

--- qe-admin/src/pages/TeacherBatchList/components/SchoolChangeContainer.tsx
+++ qe-admin/src/pages/TeacherBatchList/components/SchoolChangeContainer.tsx
@@ -1,4 +1,5 @@
-import { teacherBatchesView } from '@/services/ant-design-pro/api';
+import { addTeacherSchedule, teacherBatchesView } from '@/services/ant-design-pro/api';
+import { handleAPIResponse } from '@/services/ant-design-pro/helpers';
 import { EditOutlined } from '@ant-design/icons'
 import { Button, Modal, Select } from 'antd'
 import React, { useState } from 'react'
@@ -9,7 +10,6 @@ export type UpdateFormProps = {
 };
 
 const SchoolChangeContainer: React.FC<UpdateFormProps> = ({ teacher, schools }) => {
-    // console.log('TEACHER ---->>', JSON.stringify(teacher))
     const [container, setContainer] = useState<boolean>(false);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [selectedSchool, setSelectedSchool] = useState<any>(teacher?.school?.id);
@@ -23,18 +23,79 @@ const SchoolChangeContainer: React.FC<UpdateFormProps> = ({ teacher, schools }) 
         }
         setError(undefined)
         setIsLoading(true)
+        let msg: any;
         try {
-            let msg: any = await teacherBatchesView(teacher.id);
-            const existingData = msg.data;
-            console.log(existingData)
-            if (existingData.schoolId === selectedSchool) {
-                setError('Please change the school')
-                setIsLoading(false)
-                return;
-            }
-            existingData.schoolId = selectedSchool
-
+            msg = await teacherBatchesView(teacher.id);
         } catch (error) {
+            handleAPIResponse({ status: 400 }, "School Updated Successfully", "Failed To Update School");
+            setIsLoading(false)
+            return;
+        }
+        const existingData = msg.data;
+        if (existingData.schoolId === selectedSchool) {
+            setError('Please change the school')
+            setIsLoading(false)
+            return;
+        }
+        existingData.schoolId = selectedSchool
+        const dataForm: any = {
+            id: existingData.id,
+            teacherId: existingData.teacherId,
+            batchCode: existingData.batchCode,
+            category: existingData.category,
+            firstName: existingData.firstName,
+            lastName: existingData.lastName,
+            dob: existingData.dob,
+            phoneNumber: existingData.phoneNumber,
+            email: existingData.email,
+            address: existingData.address,
+            whatsapp: existingData.whatsapp,
+            gender: existingData.gender,
+            languages: existingData.languages,
+            startDate: existingData.startDate,
+            type: existingData.type,
+            photo: existingData.photo,
+            status: existingData.status,
+            schoolId: existingData.schoolId,
+            leadAvailability: [],
+            lead: [],
+        };
+        if (existingData?.teacher && existingData?.teacher[0]) {
+            dataForm.lead = [
+                {
+                    resume: existingData?.teacher[0].resume,
+                    qualification: existingData?.teacher &&
+                        existingData?.teacher.map(function (lead: { qualification: any; }, i: any) {
+                            return lead.qualification;
+                        }),
+                    totalexp: existingData?.teacher &&
+                        existingData?.teacher.map(function (lead: { totalexp: any; }, i: any) {
+                            return lead.totalexp;
+                        }),
+                    video: existingData?.teacher[0]?.video,
+                    certificates: existingData?.teacher[0]?.certificates,
+                    joiningdate: existingData?.teacher &&
+                        existingData?.teacher.map(function (lead: { joiningdate: any; }, i: any) {
+                            return lead.joiningdate;
+                        }),
+                    ratings: 1,
+                    classestaken: 10,
+                    teachertype: "teacher",
+                },
+            ]
+        }
+        try {
+            // 登录
+            const msg = await addTeacherSchedule({
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(dataForm),
+            });
+
+            handleAPIResponse(msg, "School Updated Successfully", "Failed To Update School");
+        } catch (error) {
+            handleAPIResponse({ status: 400 }, "School Updated Successfully", "Failed To Update School");
         }
         setIsLoading(false)
     }
