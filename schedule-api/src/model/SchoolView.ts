b2128f8684e234f790b135b468f3739ba9d41c6a
Manish Prajapati <manish.prajapati@queensenglish.co>
2023-04-18 16:45:54 +0530

    Added schoolId and location code to schoolView Class

--- schedule-api/src/model/SchoolView.ts
+++ schedule-api/src/model/SchoolView.ts
@@ -2,32 +2,50 @@ import { SRA } from "../entity/SRA";
 import { Classes } from "../entity/Classes";
 
 export class SchoolView {
-    id: string;
-    schoolName?: string;
-    schoolCode: string;
-    poc?: string;
-    sraName?: string;
-    sra?: SRA;
-    classes?: Classes[];
-    classesData?: any[];
-    createdAt?: Date | string;
-    schoolStatus?: string;
-    numberOfBatches?: number;
-    location?: string;
+  id: string;
+  schoolId: string;
+  schoolName?: string;
+  schoolCode: string;
+  locationCode: string;
+  poc?: string;
+  sraName?: string;
+  sra?: SRA;
+  classes?: Classes[];
+  classesData?: any[];
+  createdAt?: Date | string;
+  schoolStatus?: string;
+  numberOfBatches?: number;
+  location?: string;
 
-    public constructor(id: string, schoolName?: string, schoolCode?: string, poc?: string, sraName?: string, sra?: SRA, classes?: Classes[], classesData?: any[], createdAt?: Date | string, schoolStatus?: string, numberOfBatches?: number, location?: string) {
-        this.id = id;
-        this.schoolName = schoolName;
-        this.schoolCode = schoolCode;
-        this.poc = poc;
-        this.sraName = sraName;
-        this.sra = sra;
-        this.classes = classes;
-        this.classesData = classesData;
-        this.createdAt = createdAt;
-        this.schoolStatus = schoolStatus;
-        this.numberOfBatches = numberOfBatches;
-        this.location = location;
-    }
-
+  public constructor(
+    id: string,
+    schoolId?: string,
+    schoolName?: string,
+    schoolCode?: string,
+    locationCode?: string,
+    poc?: string,
+    sraName?: string,
+    sra?: SRA,
+    classes?: Classes[],
+    classesData?: any[],
+    createdAt?: Date | string,
+    schoolStatus?: string,
+    numberOfBatches?: number,
+    location?: string
+  ) {
+    this.id = id;
+    this.schoolId = schoolId;
+    this.schoolName = schoolName;
+    this.schoolCode = schoolCode;
+    this.locationCode = locationCode;
+    this.poc = poc;
+    this.sraName = sraName;
+    this.sra = sra;
+    this.classes = classes;
+    this.classesData = classesData;
+    this.createdAt = createdAt;
+    this.schoolStatus = schoolStatus;
+    this.numberOfBatches = numberOfBatches;
+    this.location = location;
+  }
 }
