9377713212fd0ae874309881258732abb937df14
Manish Prajapati <manish.prajapati@queensenglish.co>
2023-05-25 18:54:29 +0530

    Separated sheet links

--- qe-admin/config/constants.ts
+++ qe-admin/config/constants.ts
@@ -5,7 +5,7 @@ export const ALLOWED_AUTO_VERIFY_PAYMENTS = ["Razorpay", "Cashfree"];
 
 export const SPREADSHEETS = {
   STUDENT_BULK_UPLOAD:
-    "https://docs.google.com/spreadsheets/d/17ZF9SB3u_tj5RCbiMYw4BEEwiG4Z_IjlwrPd5KnB82c/edit?usp=sharing",
+    "https://docs.google.com/spreadsheets/d/1SI4ny5hReFKya7zUBUbhgZ0GSTpArcRy1rRLnn1fUSs/edit?usp=sharing",
   TEACHER_BULK_UPLOAD:
     "https://docs.google.com/spreadsheets/d/17ZF9SB3u_tj5RCbiMYw4BEEwiG4Z_IjlwrPd5KnB82c/edit?usp=sharing",
 };
