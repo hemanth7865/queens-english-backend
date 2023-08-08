185379f4c414cd479f0db3e5d62c9247856ce12d
Manish Prajapati <manish.prajapati@queensenglish.co>
2023-08-08 18:16:58 +0530

    Created Service file for CSV Upload Record

---
+++ schedule-api/src/services/CSVUploadService.ts
@@ -0,0 +1,34 @@
+import { getConnection, getRepository } from "typeorm";
+import { CSVUpload } from "../entity/CSVUpload";
+
+export class CSVUploadService {
+  private csvUploadRepository = getRepository(CSVUpload);
+
+  CSVUploadService() {}
+
+  async createCsvUploadRecord(
+    data: {
+      id?: string;
+      schoolId?: string;
+      fileName?: string;
+      errors?: any[];
+    },
+    userId: string
+  ) {
+    const { id, schoolId, fileName, errors } = data;
+    let csvUploadDetails;
+    if (id) {
+      csvUploadDetails = this.csvUploadRepository.findOne({ id: id });
+    } else {
+      csvUploadDetails = new CSVUpload();
+      csvUploadDetails.uploadedAt = new Date();
+    }
+    csvUploadDetails.schoolId = schoolId;
+    csvUploadDetails.fileName = fileName;
+    csvUploadDetails.errors = errors ? JSON.stringify(errors) : null;
+    csvUploadDetails.uploadedBy = userId;
+
+    const response = await this.csvUploadRepository.save(csvUploadDetails);
+    return response;
+  }
+}
