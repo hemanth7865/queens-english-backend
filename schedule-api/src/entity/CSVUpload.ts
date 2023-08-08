7e4a8dd8cee95f07a34b7dd8c93aeed5e6ab1492
Manish Prajapati <manish.prajapati@queensenglish.co>
2023-08-08 16:20:30 +0530

    Created entity for CSV Upload table

---
+++ schedule-api/src/entity/CSVUpload.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, BaseEntity, Column } from "typeorm";
+
+@Entity("csv_upload_details")
+export class CSVUpload extends BaseEntity {
+  CSVUpload() {}
+  @PrimaryGeneratedColumn("uuid")
+  id: number;
+
+  @Column({ nullable: true, type: "text" })
+  schoolId: string;
+
+  @Column({ nullable: true, type: "text" })
+  fileName: string;
+
+  @Column({ nullable: true, type: "text" })
+  errors: string;
+
+  @Column({ nullable: true, type: "text" })
+  uploadedBy: string;
+
+  @Column({ nullable: true, type: "datetime" })
+  uploadedAt: Date;
+}
