ef7a2bd12bb37704cb86a7fe1f5fb29d13393751
Manish Prajapati <manish.prajapati@queensenglish.co>
2023-08-10 17:44:31 +0530

    Storing files in test folder when environment is not production

--- schedule-api/src/helpers/AWSClient.ts
+++ schedule-api/src/helpers/AWSClient.ts
@@ -3,6 +3,7 @@ import { v4 as uuid } from "uuid";
 
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID!;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY!;
+const devEnvironment = process.env.NODE_ENV !== "production";
 
 const s3Client = new S3Client({
   region: "ap-south-1",
@@ -16,7 +17,7 @@ export const uploadFile = async (
   file,
   BucketName: string = "eqbulkuploads"
 ) => {
-  const fileName = `${uuid()}-${file.name}`;
+  const fileName = `${devEnvironment ? "test/" : ""}${uuid()}-${file.name}`;
   const command = new PutObjectCommand({
     Bucket: BucketName,
     Key: fileName,
