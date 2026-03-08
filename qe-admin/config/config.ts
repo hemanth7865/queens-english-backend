bddc72008820d85ff6379698866d806bea04a758
Hemanth N <hemu9740@gmail.com>
2026-03-08 20:51:46 +0530

    Refactor config by removing unused settings
    Removed locale and inspectorConfig settings from config.

--- qe-admin/config/config.ts
+++ qe-admin/config/config.ts
@@ -2,15 +2,9 @@ import { defineConfig } from "umi";
 
 export default defineConfig({
 
-  locale: {
-    default: "en-US",
-    antd: true,
-    baseNavigator: true,
-  },
-
-  access: {},
-
-  model: {},
+  plugins: [
+    "react-dev-inspector/plugins/umi/react-inspector",
+  ],
 
   define: {
     AZURE_CODE: "3oRefSONemrd2HatnbHfHLfPMat2fgi2kakJHrCDHhXbmhfDSQ6r8Q==",
@@ -19,16 +13,6 @@ export default defineConfig({
     GOOGLE_CLIENT_ID: "GOOGLE_CLIENT_ID",
     BLOB_URL: "BLOB_URL",
     BLOB_SAS: "BLOB_SAS",
-  },
-
-  plugins: [
-    "react-dev-inspector/plugins/umi/react-inspector",
-  ],
-
-  inspectorConfig: {
-    exclude: [],
-    babelPlugins: [],
-    babelOptions: {},
   },
 
 });
