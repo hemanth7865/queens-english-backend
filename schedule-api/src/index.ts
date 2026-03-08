18600d4225c244de858021ae519d4f4e7e5cc691
Hemanth N <hemu9740@gmail.com>
2026-03-08 03:02:16 +0000

    Fix: Remove TypeORM connection, use only Cosmos DB

--- schedule-api/src/index.ts
+++ schedule-api/src/index.ts
@@ -1,18 +1,14 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { Routes } from "./routes";
-import { User } from "./entity/User";
 import * as CookieParser from "cookie-parser";
 var cors = require("cors");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const fileUpload = require('express-fileupload');
 import * as utils from './utils/payment/RazorPayUtils';
-import { MONGOOSE_OPTIONS } from "./helpers/Constants";
-const mongoose = require("mongoose");
 
 export default { utils }
 
@@ -28,7 +24,7 @@ app.use(CookieParser(process.env.JWT_TOKEN_SECRET));
 app.options("*", cors());
 app.use(cors());
 
-// Start server immediately (don't wait for DB)
+// Start server immediately
 app.listen(3000, () => {
   console.log("Express server has started on port 3000");
 });
@@ -38,53 +34,44 @@ if (process.env.NODE_ENV === "production") {
   console.log = function () {};
 }
 
-// Try to connect to database (optional - won't crash app if it fails)
-createConnection()
-  .then(async (connection) => {
-    console.log("Database connected successfully");
-    
-    // register express routes from defined application routes
-    Routes.forEach((route) => {
-      (app as any)[route.method](
-        route.route,
-        route.authenticate ? authenticateToken : bypassAuth,
-        route.apiKey ? authenticateAPIKey : bypassAuth,
-        (req: Request, res: Response, next: Function) => {
-          const result = new (route.controller as any)()[route.action](
-            req,
-            res,
-            next
-          );
-          if (result instanceof Promise) {
-            result
-              .then((result) => {
-                if (result !== null && result !== undefined) {
-                  try {
-                    if (typeof result === "object") {
-                      res.json(JSON.parse(JSON.stringify(result)));
-                    } else {
-                      res.send(`${result}`);
-                    }
-                  } catch (_) {
-                    if (!res.headersSent) {
-                      res.send("Internal server error");
-                    }
-                  }
-                }
-              })
-              .catch((_) => {
-                if (!res.headersSent) {
-                  res.status(500).send("Internal server error");
-                }
-              });
-          }
-        }
+// Register routes (without database connection)
+Routes.forEach((route) => {
+  (app as any)[route.method](
+    route.route,
+    route.authenticate ? authenticateToken : bypassAuth,
+    route.apiKey ? authenticateAPIKey : bypassAuth,
+    (req: Request, res: Response, next: Function) => {
+      const result = new (route.controller as any)()[route.action](
+        req,
+        res,
+        next
       );
-    });
-  })
-  .catch((error) => {
-    console.log("⚠️ Database connection failed (app will still run):", error.message);
-  });
+      if (result instanceof Promise) {
+        result
+          .then((result) => {
+            if (result !== null && result !== undefined) {
+              try {
+                if (typeof result === "object") {
+                  res.json(JSON.parse(JSON.stringify(result)));
+                } else {
+                  res.send(`${result}`);
+                }
+              } catch (_) {
+                if (!res.headersSent) {
+                  res.send("Internal server error");
+                }
+              }
+            }
+          })
+          .catch((_) => {
+            if (!res.headersSent) {
+              res.status(500).send("Internal server error");
+            }
+          });
+      }
+    }
+  );
+});
 
 function bypassAuth(req, res, next) {
     const authHeader = req.headers["authorization"];
@@ -121,9 +108,6 @@ function authenticateToken(req, res, next) {
   });
 }
 
-/**
- * Middleware to authenticate API key 
- */
 function authenticateAPIKey(req, res, next) {
   const apiKey = req.query.apiKey;
   if (apiKey != process.env.API_KEY) {
