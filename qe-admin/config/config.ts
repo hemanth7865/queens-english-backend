31b4b97f736eb80f384ac8765e0ba809855ce7c8
Hemanth N <hemu9740@gmail.com>
2026-03-08 23:42:05 +0530

    Update AZURE_URL to use relative path

--- qe-admin/config/config.ts
+++ qe-admin/config/config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 
   define: {
     AZURE_CODE: "3oRefSONemrd2HatnbHfHLfPMat2fgi2kakJHrCDHhXbmhfDSQ6r8Q==",
-    AZURE_URL: "https://ed-uat-functions.azurewebsites.net/",
+    AZURE_URL: "/api/",
     ZOOM_GENERIC_LINK: "https://admin.thequeensenglish.co/be/",
     GOOGLE_CLIENT_ID: "GOOGLE_CLIENT_ID",
     BLOB_URL: "BLOB_URL",
